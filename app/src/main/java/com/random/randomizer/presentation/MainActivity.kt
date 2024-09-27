77519e0f382b622d563271e0b69f06819f56e998
EvgenyShpagin <shpagin.from.usa@gmail.com>
2024-09-27 11:40:02 +0700

    refactor: rename app theme

--- app/src/main/java/com/random/randomizer/presentation/MainActivity.kt
+++ app/src/main/java/com/random/randomizer/presentation/MainActivity.kt
@@ -5,14 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.random.randomizer.presentation.home.HomeScreen
-import com.random.randomizer.presentation.theme.AppTheme
+import com.random.randomizer.presentation.theme.RandomizerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AppTheme {
+            RandomizerTheme {
                 HomeScreen()
             }
         }
