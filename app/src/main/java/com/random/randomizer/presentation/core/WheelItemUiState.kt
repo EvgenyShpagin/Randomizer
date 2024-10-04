cf5c3700ccf7501ca53aa077400c25c576e9310e
EvgenyShpagin <shpagin.from.usa@gmail.com>
2024-10-04 11:34:28 +0700

    perf: mark state data classes as immutable

--- app/src/main/java/com/random/randomizer/presentation/core/WheelItemUiState.kt
+++ app/src/main/java/com/random/randomizer/presentation/core/WheelItemUiState.kt
@@ -1,8 +1,10 @@
 package com.random.randomizer.presentation.core
 
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 
+@Immutable
 data class WheelItemUiState(
     val id: Int,
     val title: String,
--- app/src/main/java/com/random/randomizer/presentation/screen/home/HomeUiState.kt
+++ app/src/main/java/com/random/randomizer/presentation/screen/home/HomeUiState.kt
@@ -1,7 +1,9 @@
 package com.random.randomizer.presentation.screen.home
 
+import androidx.compose.runtime.Immutable
 import com.random.randomizer.presentation.core.WheelItemUiState
 
+@Immutable
 data class HomeUiState(
     val wheelItems: List<WheelItemUiState> = emptyList()
 )
