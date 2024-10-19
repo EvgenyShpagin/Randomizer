ca67fcc953847222947e7404a9fb2b74355ca226
EvgenyShpagin <shpagin.from.usa@gmail.com>
2024-10-19 16:45:13 +0700

    refactor: change description type from String? to String

--- domain/src/main/java/com/random/randomizer/domain/model/WheelSegment.kt
+++ domain/src/main/java/com/random/randomizer/domain/model/WheelSegment.kt
@@ -3,7 +3,7 @@ package com.random.randomizer.domain.model
 data class WheelSegment(
     val id: Int,
     val title: String,
-    val description: String?,
+    val description: String,
     val thumbnailPath: String?,
     val customColor: Long?
 )
