ac5f2a43847d1f2338f9a85ee4652629391369e3
EvgenyShpagin <shpagin.from.usa@gmail.com>
2024-10-31 15:31:08 +0700

    test(presentation/core): add unit tests for mappers

---
+++ app/src/test/java/com/random/randomizer/presentation/core/MappersTest.kt
@@ -0,0 +1,67 @@
+package com.random.randomizer.presentation.core
+
+import androidx.compose.ui.graphics.Color
+import com.random.randomizer.domain.model.WheelSegment
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class MappersTest {
+
+    @Test
+    fun presentationColor_to_domainColor_mapsCorrectly() {
+        val expectedDomainColor = 0xFFFF0000
+        val actualPresentationColorLong = Color.Red.valueAsLong()
+        assertEquals(expectedDomainColor, actualPresentationColorLong)
+    }
+
+    @Test
+    fun domainColor_to_presentationColor_mapsCorrectly() {
+        val expectedPresentationColor = Color.Red
+        val actualPresentationColor = Color(0xFFFF0000)
+        assertEquals(expectedPresentationColor, actualPresentationColor)
+    }
+
+    @Test
+    fun domainWheelSegment_to_presentationWheelSegmentUiState_mapsCorrectly() {
+        val domain = WheelSegment(
+            id = 5,
+            title = "Some title",
+            description = "Some description",
+            thumbnailPath = null,
+            customColor = 0x80FAFAFA
+        )
+
+        val presentationUiState = domain.toUiState()
+
+        assertEquals(domain.id, presentationUiState.id)
+        assertEquals(domain.title, presentationUiState.title)
+        assertEquals(domain.description, presentationUiState.description)
+        assertEquals(null, presentationUiState.image)
+        assertEquals(
+            domain.customColor,
+            presentationUiState.customColor?.value?.shr(32)?.toLong()
+        )
+    }
+
+    @Test
+    fun presentationWheelSegmentUiState_to_domainWheelSegment_mapsCorrectly() {
+        val presentationUiState = WheelSegmentUiState(
+            id = 5,
+            title = "Some title",
+            description = "Some description",
+            image = null,
+            customColor = Color.Red
+        )
+
+        val domain = presentationUiState.toDomain(null)
+
+        assertEquals(presentationUiState.id, domain.id)
+        assertEquals(presentationUiState.title, domain.title)
+        assertEquals(presentationUiState.description, domain.description)
+        assertEquals(presentationUiState.image, null)
+        assertEquals(
+            presentationUiState.customColor?.valueAsLong(),
+            domain.customColor
+        )
+    }
+}
