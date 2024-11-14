9787c116984450b229bffaf54b4e9461e6abd286
EvgenyShpagin <shpagin.from.usa@gmail.com>
2024-11-14 11:22:21 +0700

    test: add list extension validation

---
+++ app/src/test/java/com/random/randomizer/presentation/screen/spin/UtilsTest.kt
@@ -0,0 +1,50 @@
+package com.random.randomizer.presentation.screen.spin
+
+import com.random.randomizer.presentation.core.WheelSegmentUiState
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class UtilsTest {
+
+    @Test
+    fun extendTo_doublesList_whenDoubleExtended() {
+        val originSegments = List(5) { i ->
+            WheelSegmentUiState(i, "fake$i", "", null, null)
+        }
+        val extendedSegments = originSegments.extendTo(10)
+        assertEquals(10, extendedSegments.count())
+    }
+
+    @Test
+    fun extendTo_preservesOriginalItems_whenExtended() {
+        val originSegments = List(6) { i ->
+            WheelSegmentUiState(i, "fake$i", "", null, null)
+        }
+        val extendedSegments = originSegments.extendTo(12)
+
+        assertEquals(originSegments, extendedSegments.subList(0, 6))
+        assertEquals(originSegments, extendedSegments.subList(6, 12))
+    }
+
+    @Test
+    fun extendTo_roundsUpSize_whenSizeIsNotPerfectMultiple() {
+        val originSegments = List(7) { i ->
+            WheelSegmentUiState(i, "fake$i", "", null, null)
+        }
+
+        val extendedSegments = originSegments.extendTo(8)
+
+        assertEquals(14, extendedSegments.count())
+    }
+
+    @Test
+    fun extendTo_keepsSameList_whenMinCountLessThanOriginalSize() {
+        val originSegments = List(8) { i ->
+            WheelSegmentUiState(i, "fake$i", "", null, null)
+        }
+
+        val extendedSegments = originSegments.extendTo(2)
+
+        assertEquals(8, extendedSegments.count())
+    }
+}
