8abc317af07748107e290be4f1d3f8e09e898e40
EvgenyShpagin <shpagin.from.usa@gmail.com>
2024-12-18 15:06:11 +0700

    refactor: use alias in plugins block

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false
+    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    alias(libs.plugins.hilt) apply false
 }
