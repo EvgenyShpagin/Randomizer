ed0f6c04f9e572e3c3c1c30e8189d2bfe2019723
EvgenyShpagin <shpagin.from.usa@gmail.com>
2024-12-18 14:57:32 +0700

    refactor: remove *rules.pro files

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -11,20 +11,8 @@ android {
 
     defaultConfig {
         minSdk = libs.versions.minSdk.get().toInt()
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
