fd186aa82eb171d8faa50d3f307144ba78132a10
EvgenyShpagin <shpagin.from.usa@gmail.com>
2024-12-18 14:42:13 +0700

    refactor: put sdk versions in libs.versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.random.randomizer"
-    compileSdk = 34
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.random.randomizer"
-        minSdk = 24
-        targetSdk = 34
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
