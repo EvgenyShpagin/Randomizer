26b9c5b07d249c73588f4494aa10c3466e0722de
EvgenyShpagin <shpagin.from.usa@gmail.com>
2024-12-19 17:34:45 +0700

    test: add test activity
    HiltTestActivity ensures isolation of test dependencies using Hilt.

---
+++ app/src/debug/java/com/random/randomizer/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.random.randomizer
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : ComponentActivity()
