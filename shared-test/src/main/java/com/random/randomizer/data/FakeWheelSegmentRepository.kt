67b1e7464b85425a9c0affbc29a778fefc4dac4d
EvgenyShpagin <shpagin.from.usa@gmail.com>
2024-12-25 09:55:37 +0700

    test: add required implementation for FakeWheelSegmentRepository

--- shared-test/src/main/java/com/random/randomizer/data/FakeWheelSegmentRepository.kt
+++ shared-test/src/main/java/com/random/randomizer/data/FakeWheelSegmentRepository.kt
@@ -1,17 +1,23 @@
 package com.random.randomizer.data
 
+import androidx.annotation.VisibleForTesting
 import com.random.randomizer.domain.model.WheelSegment
 import com.random.randomizer.domain.repository.WheelSegmentRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 class FakeWheelSegmentRepository @Inject constructor() : WheelSegmentRepository {
+
+    private val wheelSegments = MutableStateFlow(emptyList<WheelSegment>())
+
     override suspend fun add(segment: WheelSegment) {
-        TODO("Not yet implemented")
+        wheelSegments.update { segments -> segments + segment }
     }
 
     override suspend fun getAll(): List<WheelSegment> {
-        TODO("Not yet implemented")
+        return wheelSegments.value
     }
 
     override fun getAllStream(): Flow<List<WheelSegment>> {
@@ -32,5 +38,12 @@ class FakeWheelSegmentRepository @Inject constructor() : WheelSegmentRepository 
 
     override suspend fun deleteById(segmentId: Int) {
         TODO("Not yet implemented")
+    }
+}
+
+@VisibleForTesting
+suspend fun WheelSegmentRepository.addMultiple(segments: List<WheelSegment>) {
+    segments.forEach { segment ->
+        add(segment)
     }
 }
