809a17472de3df6d101747021eef32c7671d4a48
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-01-02 13:32:21 +0700

    test: add test for making unique

--- app/src/test/java/com/random/randomizer/presentation/screen/edit/EditViewModelTest.kt
+++ app/src/test/java/com/random/randomizer/presentation/screen/edit/EditViewModelTest.kt
@@ -4,6 +4,7 @@ import com.random.randomizer.MainCoroutineRule
 import com.random.randomizer.data.FakeThumbnailRepository
 import com.random.randomizer.data.FakeWheelSegmentRepository
 import com.random.randomizer.domain.model.WheelSegment
+import com.random.randomizer.domain.model.contentEquals
 import com.random.randomizer.domain.usecase.CreateWheelSegmentUseCase
 import com.random.randomizer.domain.usecase.DeleteThumbnailUseCase
 import com.random.randomizer.domain.usecase.DeleteWheelSegmentUseCase
@@ -13,6 +14,7 @@ import com.random.randomizer.domain.usecase.ValidateWheelSegmentUseCase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
@@ -20,6 +22,7 @@ import org.junit.Test
 
 
 private val EmptyWheelSegment = WheelSegment(1, "", "", null, null)
+private val NonEmptyWheelSegment = WheelSegment(1, "Title", "Description", null, null)
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class EditViewModelTest {
@@ -109,5 +112,28 @@ class EditViewModelTest {
 
         // Then - verify just created blank wheel segment was deleted
         assertTrue(wheelSegmentRepository.getAll().isEmpty())
+    }
+
+    @Test
+    fun makesUnique_onFinishEdit() = runTest {
+        // Given - one wheel segment
+        wheelSegmentRepository.add(NonEmptyWheelSegment)
+
+        // When - on create new segment
+        viewModel.onEvent(EditUiEvent.CreateSegment)
+        // after creating a copy of already existing wheel segment
+        val createdWheelSegmentId = viewModel.uiState.value.currentlyEditedSegmentId!!
+        val duplicate = NonEmptyWheelSegment.copy(id = createdWheelSegmentId)
+        // update wheel segment being edited to duplicate
+        wheelSegmentRepository.update(duplicate)
+        // and finish edit
+        viewModel.onEvent(EditUiEvent.FinishSegmentEdit)
+
+        val savedWheelSegments = wheelSegmentRepository.getAll()
+
+        // Then - verify 2 wheel segments are saved
+        assertEquals(2, savedWheelSegments.count())
+        // and they are different
+        assertFalse(savedWheelSegments[0].contentEquals(savedWheelSegments[1]))
     }
 }
