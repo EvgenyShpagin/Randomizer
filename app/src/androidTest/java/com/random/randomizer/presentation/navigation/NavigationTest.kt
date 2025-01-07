21b3f478265ade3e44dcfa519995cf7ee799439d
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-01-07 12:09:58 +0700

    test(app): refactor

--- app/src/androidTest/java/com/random/randomizer/presentation/navigation/NavigationTest.kt
+++ app/src/androidTest/java/com/random/randomizer/presentation/navigation/NavigationTest.kt
@@ -1,6 +1,5 @@
 package com.random.randomizer.presentation.navigation
 
-import androidx.activity.compose.setContent
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
@@ -10,8 +9,8 @@ import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.filters.LargeTest
+import com.random.randomizer.HiltTestActivity
 import com.random.randomizer.R
-import com.random.randomizer.presentation.core.MainActivity
 import com.random.randomizer.test_util.stringResource
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -27,19 +26,14 @@ class NavigationTest {
     val hiltRule = HiltAndroidRule(this)
 
     @get:Rule(order = 1)
-    val composeTestRule = createAndroidComposeRule<MainActivity>()
+    val composeTestRule = createAndroidComposeRule<HiltTestActivity>()
 
-    lateinit var navController: TestNavHostController
+    private lateinit var navController: TestNavHostController
 
 
     @Before
-    fun setup() {
-        hiltRule.inject()
-        setupNavHost()
-    }
-
     fun setupNavHost() {
-        composeTestRule.activity.setContent {
+        composeTestRule.setContent {
             navController = TestNavHostController(LocalContext.current).apply {
                 navigatorProvider.addNavigator(ComposeNavigator())
             }
@@ -56,7 +50,7 @@ class NavigationTest {
     }
 
     @Test
-    fun navHost_navigatesToEdit_whenEditButtonClicked() {
+    fun homeScreen_navigatesToEdit_whenEditButtonClicked() {
         val editButtonContentDescription = stringResource(R.string.cd_edit_wheel_segments)
         val editTopBarText = stringResource(R.string.edit_screen_title)
         composeTestRule
