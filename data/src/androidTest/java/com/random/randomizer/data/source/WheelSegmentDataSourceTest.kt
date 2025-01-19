02040b548589a390e6594349587d2f5c3317ec14
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-01-19 11:21:22 +0700

    test(data): add tests for base functionality
    Base functionality is two methods: getById and upsert

---
+++ data/src/androidTest/java/com/random/randomizer/data/source/WheelSegmentDataSourceTest.kt
@@ -0,0 +1,57 @@
+package com.random.randomizer.data.source
+
+import android.graphics.Bitmap
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import com.random.randomizer.data.util.toByteArray
+import com.random.randomizer.domain.model.Image
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class WheelSegmentDataSourceTest {
+
+    // Subject under test
+    lateinit var dataSource: WheelSegmentDataSource
+
+    // using an in-memory database to store data while process is running
+    private lateinit var database: WheelSegmentDatabase
+
+    @Before
+    fun setup() {
+        database = Room.inMemoryDatabaseBuilder(
+            getApplicationContext(),
+            WheelSegmentDatabase::class.java
+        ).allowMainThreadQueries().build()
+
+        dataSource = WheelSegmentDataSource(
+            context = getApplicationContext(),
+            wheelSegmentDao = database.wheelSegmentDao
+        )
+    }
+
+    @Test
+    fun upsertAndGetById() = runTest {
+        // Given - wheel segment in database
+        val wheelSegment = WheelSegment(
+            id = 1,
+            title = "Title 1",
+            description = "Description 1",
+            thumbnail = createImage(),
+            customColor = 0x112233
+        )
+        dataSource.upsert(wheelSegment)
+
+        // When - get the wheel segment by id
+        val loadedWheelSegment = dataSource.getById(wheelSegment.id)
+
+        // Then - the loaded wheel segment is equal to the expected
+        assertEquals(wheelSegment, loadedWheelSegment)
+    }
+
+    private fun createImage(): Image {
+        val bitmap = Bitmap.createBitmap(4, 4, Bitmap.Config.ARGB_8888)
+        return Image(id = "image.png", data = bitmap.toByteArray())
+    }
+}
