b1d2f02d3e35ebac2933856fee9448187b149f4c
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-01-19 11:21:37 +0700

    feat(data): add base functionality
    Base functionality is two methods: getById and upsert

---
+++ data/src/main/java/com/random/randomizer/data/source/WheelSegmentDataSource.kt
@@ -0,0 +1,111 @@
+package com.random.randomizer.data.source
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Bitmap.CompressFormat.PNG
+import com.random.randomizer.data.util.toBitmap
+import com.random.randomizer.domain.model.Image
+import java.io.File
+import java.io.FileOutputStream
+
+class WheelSegmentDataSource(
+    context: Context,
+    private val wheelSegmentDao: WheelSegmentDao
+) {
+    private val imageSaveDirectory = context.filesDir
+
+    suspend fun getById(id: Int): WheelSegment? {
+        val wheelSegment = wheelSegmentDao.getById(id) ?: return null
+        if (wheelSegment.thumbnail == null) {
+            return wheelSegment
+        } else {
+            val thumbnail = getThumbnail(wheelSegment.thumbnail.id)
+            return wheelSegment.copy(thumbnail = thumbnail)
+        }
+    }
+
+    private fun getThumbnail(thumbnailId: String): Image? {
+        val imageFile = File(imageSaveDirectory, thumbnailId)
+        return if (imageFile.exists()) {
+            val thumbnailData = imageFile.readBytes()
+            Image(thumbnailId, thumbnailData)
+        } else {
+            null
+        }
+    }
+
+    suspend fun upsert(wheelSegment: WheelSegment): Long {
+        val currentWheelSegment = getById(wheelSegment.id)
+
+        return when {
+            isThumbnailChanged(currentWheelSegment, wheelSegment) -> {
+                val savedThumbnail = saveThumbnail(wheelSegment.thumbnail!!)
+                    ?: throw IllegalStateException("Failed to save image")
+                val wheelSegmentWithSavedThumbnail = wheelSegment.copy(thumbnail = savedThumbnail)
+                wheelSegmentDao.upsert(wheelSegmentWithSavedThumbnail)
+            }
+
+            isThumbnailRemoved(currentWheelSegment, wheelSegment) -> {
+                deleteThumbnail(currentWheelSegment!!.thumbnail!!)
+                wheelSegmentDao.upsert(wheelSegment)
+            }
+
+            else -> wheelSegmentDao.upsert(wheelSegment)
+        }
+    }
+
+    private fun isThumbnailRemoved(
+        currentWheelSegment: WheelSegment?,
+        newWheelSegment: WheelSegment
+    ): Boolean {
+        return currentWheelSegment?.thumbnail != null && newWheelSegment.thumbnail == null
+    }
+
+    private fun isThumbnailChanged(
+        currentWheelSegment: WheelSegment?,
+        newWheelSegment: WheelSegment
+    ): Boolean {
+        return newWheelSegment.thumbnail != null &&
+                currentWheelSegment?.thumbnail != newWheelSegment.thumbnail
+    }
+
+    private fun saveThumbnail(image: Image): Image? {
+        require(!image.id.contains(ProhibitedSymbolRegex)) {
+            "id must not contain prohibited symbols"
+        }
+        val bitmap = image.toBitmap()
+        val filename = image.id.withExtension()
+        val isSaved = saveBitmap(bitmap, filename)
+        return if (isSaved) {
+            image.copy(id = filename)
+        } else {
+            null
+        }
+    }
+
+    private fun deleteThumbnail(thumbnail: Image): Boolean {
+        return File(imageSaveDirectory, thumbnail.id).delete()
+    }
+
+    private fun String.withExtension(): String {
+        if (endsWith(".png")) return this
+        return "$this.png"
+    }
+
+    private fun saveBitmap(bitmap: Bitmap, filename: String): Boolean {
+        val file = File(imageSaveDirectory, filename)
+        runCatching { FileOutputStream(file) }
+            .onFailure { it.printStackTrace() }
+            .onSuccess {
+                it.use { output ->
+                    val isCompressed = bitmap.compress(PNG, 100, output)
+                    return isCompressed
+                }
+            }
+        return false
+    }
+
+    private companion object {
+        val ProhibitedSymbolRegex = "[\\\\/:*?\"<>|]".toRegex()
+    }
+}
