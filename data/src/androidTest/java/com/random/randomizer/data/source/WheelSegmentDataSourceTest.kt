ce8f828c2567bd13e9a0930fb8f70881a49ea9e3
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-01-20 12:26:14 +0700

    test(data): add upsert method validation

--- data/src/androidTest/java/com/random/randomizer/data/source/WheelSegmentDataSourceTest.kt
+++ data/src/androidTest/java/com/random/randomizer/data/source/WheelSegmentDataSourceTest.kt
@@ -6,7 +6,9 @@ import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import com.random.randomizer.data.util.toByteArray
 import com.random.randomizer.domain.model.Image
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertNotEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -68,6 +70,52 @@ class WheelSegmentDataSourceTest {
         // Then - the loaded wheel segment is equal to the expected
         assertEquals(1, loadedWheelSegments.count())
         assertEquals(wheelSegment, loadedWheelSegments.single())
+    }
+
+    @Test
+    fun upsert_replacesWheelSegment_onConflict() = runTest {
+        // Given - insert a wheel segment
+        val wheelSegment = WheelSegment(
+            id = 1,
+            title = "Title 1",
+            description = "Description 1",
+            thumbnail = createImage(),
+            customColor = 0x112233
+        )
+        dataSource.upsert(wheelSegment)
+
+        // When - a wheel segment with the same id is inserted
+        val newWheelSegment = WheelSegment(
+            id = 1,
+            title = "Title 2",
+            description = "Description 2",
+            thumbnail = createImage(),
+            customColor = null
+        )
+        dataSource.upsert(newWheelSegment)
+
+        // Then - the loaded wheel segment is equal to the new
+        val loadedWheelSegment = dataSource.getById(wheelSegment.id)
+        assertNotNull(loadedWheelSegment)
+        assertEquals(newWheelSegment, loadedWheelSegment)
+    }
+
+    @Test
+    fun upsert_returnsNewId_whenWheelSegmentIdWas0() = runTest {
+        // Given - insert a wheel segment with id = 0
+        val wheelSegment = WheelSegment(
+            id = 0,
+            title = "Title 1",
+            description = "Description 1",
+            thumbnail = createImage(),
+            customColor = 0x112233
+        )
+
+        // When - save wheel segment
+        val id = database.wheelSegmentDao.upsert(wheelSegment)
+
+        // Then - wheel segment's id was updated
+        assertNotEquals(wheelSegment.id, id)
     }
 
     private fun createImage(): Image {
