68b87b3adc8384e9b17c354aee0dcce774263a7a
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-01-21 19:56:03 +0700

    feat(data): add domain-data model mappers

---
+++ data/src/main/java/com/random/randomizer/data/util/Mappers.kt
@@ -0,0 +1,40 @@
+package com.random.randomizer.data.util
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import com.random.randomizer.data.source.WheelSegment as DataSegment
+import com.random.randomizer.domain.model.WheelSegment as DomainSegment
+
+fun DataSegment.toDomain(): DomainSegment {
+    return DomainSegment(
+        id = id,
+        title = title,
+        description = description,
+        thumbnail = thumbnail,
+        customColor = customColor
+    )
+}
+
+fun List<DataSegment>.toDomain(): List<DomainSegment> {
+    return map { it.toDomain() }
+}
+
+@JvmName("segmentFlowToDomain")
+fun Flow<DataSegment>.toDomain(): Flow<DomainSegment> {
+    return map { it.toDomain() }
+}
+
+@JvmName("segmentListFlowToDomain")
+fun Flow<List<DataSegment>>.toDomain(): Flow<List<DomainSegment>> {
+    return map { it.toDomain() }
+}
+
+fun DomainSegment.toData(): DataSegment {
+    return DataSegment(
+        id = id,
+        title = title,
+        description = description,
+        thumbnail = thumbnail,
+        customColor = customColor
+    )
+}
