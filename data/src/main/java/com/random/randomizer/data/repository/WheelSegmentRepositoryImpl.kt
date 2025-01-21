72c420700563565ae4b6dc0880b283e5b6cb3f53
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-01-21 19:13:26 +0700

    feat(data): add WheelSegmentRepositoryImpl dependencies

--- data/src/main/java/com/random/randomizer/data/repository/WheelSegmentRepositoryImpl.kt
+++ data/src/main/java/com/random/randomizer/data/repository/WheelSegmentRepositoryImpl.kt
@@ -1,11 +1,21 @@
 package com.random.randomizer.data.repository
 
+import com.random.randomizer.data.di.ApplicationScope
+import com.random.randomizer.data.di.IoDispatcher
+import com.random.randomizer.data.source.WheelSegmentDataSource
 import com.random.randomizer.domain.model.WheelSegment
 import com.random.randomizer.domain.repository.WheelSegmentRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class WheelSegmentRepositoryImpl @Inject constructor() : WheelSegmentRepository {
+class WheelSegmentRepositoryImpl @Inject constructor(
+    private val dataSource: WheelSegmentDataSource,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    @ApplicationScope private val externalScope: CoroutineScope
+) : WheelSegmentRepository {
+
     override suspend fun add(segment: WheelSegment): Int {
         TODO("Not yet implemented")
     }
