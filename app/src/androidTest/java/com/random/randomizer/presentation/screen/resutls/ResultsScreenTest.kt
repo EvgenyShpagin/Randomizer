cf59f9246fdbd870c9f780af722918426d24298d
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-01-26 13:45:58 +0700

    test(app): add spin results screen tests

---
+++ app/src/androidTest/java/com/random/randomizer/presentation/screen/resutls/ResultsScreenTest.kt
@@ -0,0 +1,123 @@
+package com.random.randomizer.presentation.screen.resutls
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.lifecycle.SavedStateHandle
+import androidx.test.filters.MediumTest
+import com.random.randomizer.HiltTestActivity
+import com.random.randomizer.R
+import com.random.randomizer.data.repository.addMultiple
+import com.random.randomizer.domain.model.WheelSegment
+import com.random.randomizer.domain.repository.WheelSegmentRepository
+import com.random.randomizer.domain.usecase.DeleteWheelSegmentUseCase
+import com.random.randomizer.domain.usecase.GetWheelSegmentStreamUseCase
+import com.random.randomizer.domain.usecase.GetWheelSegmentsUseCase
+import com.random.randomizer.presentation.core.CoreMappersImpl
+import com.random.randomizer.presentation.screen.results.ResultsMappersImpl
+import com.random.randomizer.presentation.screen.results.ResultsScreen
+import com.random.randomizer.presentation.screen.results.ResultsViewModel
+import com.random.randomizer.test_util.stringResource
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import javax.inject.Inject
+
+@MediumTest
+@HiltAndroidTest
+class ResultsScreenTest {
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<HiltTestActivity>()
+
+    @Inject
+    lateinit var repository: WheelSegmentRepository
+
+    private lateinit var viewModel: ResultsViewModel
+
+    private val wheelSegmentList = List(5) { i ->
+        WheelSegment(
+            id = i,
+            title = "Title of $i wheel segment",
+            description = "Description...",
+            thumbnail = null,
+            customColor = null
+        )
+    }
+
+    private val wheelSegmentMinList = wheelSegmentList.take(2)
+
+    private val winner = wheelSegmentList[1]
+
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun showsCorrectWinnerWheelSegment() = runTest {
+        // Given - some wheel segments
+        repository.addMultiple(wheelSegmentList)
+
+        // When - on startup
+        setContent()
+
+        // Then - verify the winner wheel segment is shown
+        composeTestRule
+            .onNodeWithText(winner.title)
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun showsDeleteButton_whenMoreThanTwoWheelSegmentsExists() = runTest {
+        // Given - some wheel segments
+        repository.addMultiple(wheelSegmentList)
+
+        // When - on startup
+        setContent()
+
+        // Then - verify the "Delete and Spin" button is visible
+        composeTestRule
+            .onNodeWithText(stringResource(R.string.button_delete_and_spin))
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun hidesDeleteButton_whenOnlyTwoWheelSegmentsExists() = runTest {
+        // Given - 2 wheel segments
+        repository.addMultiple(wheelSegmentMinList)
+
+        // When - on startup
+        setContent()
+
+        // Then - verify the "Delete and Spin" button is hidden
+        composeTestRule
+            .onNodeWithText(stringResource(R.string.button_delete_and_spin))
+            .assertIsNotDisplayed()
+    }
+
+    private fun setContent() {
+        composeTestRule.setContent {
+            viewModel = ResultsViewModel(
+                savedStateHandle = SavedStateHandle(mapOf("winnerWheelSegmentId" to winner.id)),
+                getWheelSegmentStreamUseCase = GetWheelSegmentStreamUseCase(repository),
+                getWheelSegmentsUseCase = GetWheelSegmentsUseCase(repository),
+                deleteWheelSegmentUseCase = DeleteWheelSegmentUseCase(repository),
+                mappers = ResultsMappersImpl(CoreMappersImpl())
+            )
+            ResultsScreen(
+                viewModel = viewModel,
+                navigateToSpin = {},
+                navigateToHome = {}
+            )
+        }
+    }
+}
