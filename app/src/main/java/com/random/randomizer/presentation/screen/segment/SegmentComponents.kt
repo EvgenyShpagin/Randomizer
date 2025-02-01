75791acc0ce8892dd82ff6acd5f455e4cc7c0ea6
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-02-01 11:21:01 +0700

    chore(app): add WheelSegmentTopAppBar preview

--- app/src/main/java/com/random/randomizer/presentation/screen/segment/SegmentComponents.kt
+++ app/src/main/java/com/random/randomizer/presentation/screen/segment/SegmentComponents.kt
@@ -5,12 +5,15 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.random.randomizer.R
+import com.random.randomizer.presentation.theme.AppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -29,4 +32,12 @@ fun WheelSegmentTopAppBar(
         },
         modifier = modifier
     )
+}
+
+@Preview
+@Composable
+private fun WheelSegmentTopAppBarPreview() {
+    AppTheme {
+        WheelSegmentTopAppBar(onNavigationClick = {})
+    }
 }
