49c3ba790550344ce44e47753ddf5fbcec71bd30
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-02-12 13:22:51 +0700

    test(domain): add thumbnail id replacement test

--- domain/src/test/java/com/random/randomizer/domain/usecase/CreateWheelSegmentUseCaseTest.kt
+++ domain/src/test/java/com/random/randomizer/domain/usecase/CreateWheelSegmentUseCaseTest.kt
@@ -1,46 +1,52 @@
 package com.random.randomizer.domain.usecase
 
+import com.random.randomizer.data.repository.FakeWheelSegmentRepository
+import com.random.randomizer.domain.model.Image
 import com.random.randomizer.domain.model.WheelSegment
-import com.random.randomizer.domain.model.contentEquals
 import com.random.randomizer.domain.repository.WheelSegmentRepository
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.mockk
 import junit.framework.TestCase.assertEquals
-import junit.framework.TestCase.assertTrue
+import junit.framework.TestCase.assertNotNull
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertNotEquals
+import org.junit.Before
 import org.junit.Test
 
 class CreateWheelSegmentUseCaseTest {
 
-    val repository = mockk<WheelSegmentRepository>()
-    val createWheelSegmentUseCase = CreateWheelSegmentUseCase(repository)
+    private lateinit var repository: WheelSegmentRepository
+    private lateinit var createWheelSegmentUseCase: CreateWheelSegmentUseCase
 
-    @Test
-    fun addsSegment() = runTest {
-        coEvery { repository.add(any()) } returns 1
+    private val wheelSegment = WheelSegment(
+        id = 0,
+        title = "Title",
+        description = "Description",
+        thumbnail = Image("id\\with\\separators.png", byteArrayOf()),
+        customColor = null
+    )
 
-        createWheelSegmentUseCase()
-
-        coVerify { repository.add(any()) }
+    @Before
+    fun setup() {
+        repository = FakeWheelSegmentRepository()
+        createWheelSegmentUseCase = CreateWheelSegmentUseCase(repository)
     }
 
     @Test
-    fun returnsEmptySegment() = runTest {
-        coEvery { repository.add(any()) } returns 1
-
-        val actual = createWheelSegmentUseCase()
-        val expected = WheelSegment(0, "", "", null, null)
-
-        assertTrue(actual.contentEquals(expected))
+    fun addsSegment() = runTest {
+        createWheelSegmentUseCase(wheelSegment)
+        assertEquals(repository.getAll().count(), 1)
     }
 
     @Test
     fun replacesId() = runTest {
-        coEvery { repository.add(any()) } returns 1
+        val createdSegmentId = createWheelSegmentUseCase(wheelSegment)
+        assertNotEquals(wheelSegment.id, createdSegmentId)
+    }
 
-        val createdSegment = createWheelSegmentUseCase()
-
-        assertEquals(createdSegment.id, 1)
+    @Test
+    fun replacesThumbnailId() = runTest {
+        val segmentId = createWheelSegmentUseCase(wheelSegment)
+        val saved = repository.get(segmentId)
+        assertNotNull(saved?.thumbnail)
+        assertNotEquals(wheelSegment.thumbnail, saved!!.thumbnail)
     }
 }
