5d09995755d068a58cb983eb1cc882f68f64634f
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-02-12 14:06:58 +0700

    test(domain): fix UpdateWheelSegmentUseCaseTest

--- domain/src/test/java/com/random/randomizer/domain/usecase/UpdateWheelSegmentUseCaseTest.kt
+++ domain/src/test/java/com/random/randomizer/domain/usecase/UpdateWheelSegmentUseCaseTest.kt
@@ -1,5 +1,6 @@
 package com.random.randomizer.domain.usecase
 
+import com.random.randomizer.data.repository.FakeWheelSegmentRepository
 import com.random.randomizer.domain.common.Result
 import com.random.randomizer.domain.model.Image
 import com.random.randomizer.domain.model.WheelSegment
@@ -7,31 +8,41 @@ import com.random.randomizer.domain.repository.WheelSegmentRepository
 import com.random.randomizer.domain.util.ImageScaler
 import io.mockk.coEvery
 import io.mockk.coVerify
-import io.mockk.just
 import io.mockk.mockk
-import io.mockk.runs
+import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Test
 
 class UpdateWheelSegmentUseCaseTest {
 
-    private val repository = mockk<WheelSegmentRepository>()
+    private lateinit var repository: WheelSegmentRepository
     private val imageScaler = mockk<ImageScaler>()
 
+    private val wheelSegment = WheelSegment(
+        id = 1,
+        title = "Title",
+        description = "Description",
+        thumbnail = Image("id\\with\\separators.png", byteArrayOf()),
+        customColor = null
+    )
+
     // Subject under test
-    private val updateWheelSegmentUseCase = UpdateWheelSegmentUseCase(repository, imageScaler)
+    private lateinit var updateWheelSegmentUseCase: UpdateWheelSegmentUseCase
+
+    @Before
+    fun setup() {
+        repository = FakeWheelSegmentRepository()
+        updateWheelSegmentUseCase = UpdateWheelSegmentUseCase(repository, imageScaler)
+    }
 
     @Test
     fun returnsFailure_whenSegmentDoesNotExist() = runTest {
         // Given - empty repository
-        coEvery { repository.get(segmentId = 1) } returns null
 
         // When - invoke update
-        val result = updateWheelSegmentUseCase(
-            wheelSegmentId = 1,
-            transform = { segment -> segment } // update does not matter
-        )
+        val result = updateWheelSegmentUseCase(wheelSegment)
 
         // Then - verify was returned Failure
         assertTrue(result is Result.Failure)
@@ -40,37 +51,26 @@ class UpdateWheelSegmentUseCaseTest {
     @Test
     fun updatesSegment_whenSegmentExists() = runTest {
         // Given - one item in repository
-        val savedWheelSegment = WheelSegment(1, "Title", "Description", null, null)
-        val goingToBeUpdatedWheelSegment = WheelSegment(1, "Title new", "Description", null, null)
+        repository.add(wheelSegment)
 
-        coEvery { repository.get(segmentId = 1) } returns savedWheelSegment
-        coEvery { repository.update(goingToBeUpdatedWheelSegment) } just runs
+        val changedWheelSegment = wheelSegment.copy(title = "Other title")
 
         // When - invoke update
-        updateWheelSegmentUseCase(
-            wheelSegmentId = 1,
-            transform = { goingToBeUpdatedWheelSegment }
-        )
+        updateWheelSegmentUseCase(changedWheelSegment)
 
-        // Then - verify update was called with our segment
-        coVerify { repository.update(goingToBeUpdatedWheelSegment) }
+        // Then - verify item updated
+        val actual = repository.get(wheelSegment.id)
+        assertEquals(changedWheelSegment, actual)
     }
 
     @Test
     fun doesNotScale_whenImageIsTheSame() = runTest {
         // Given - one item in repository
-        val thumbnail = Image("id1", byteArrayOf())
-        val savedWheelSegment = WheelSegment(1, "Title", "Description", thumbnail, null)
-        val goingToBeUpdatedWheelSegment = WheelSegment(1, "Title", "Description", thumbnail, null)
-
-        coEvery { repository.get(segmentId = 1) } returns savedWheelSegment
-        coEvery { repository.update(goingToBeUpdatedWheelSegment) } just runs
+        repository.add(wheelSegment)
 
         // When - invoke update
-        updateWheelSegmentUseCase(
-            wheelSegmentId = 1,
-            transform = { goingToBeUpdatedWheelSegment }
-        )
+        updateWheelSegmentUseCase(wheelSegment)
+
         // Then - verify scale was not called
         coVerify(exactly = 0) { imageScaler.scale(any(), any()) }
     }
@@ -78,21 +78,15 @@ class UpdateWheelSegmentUseCaseTest {
     @Test
     fun scale_whenImageWasReplaced() = runTest {
         // Given - one item in repository
-        val oldThumbnail = Image("id1", byteArrayOf())
-        val newThumbnail = Image("id2", byteArrayOf())
-        val savedWheelSegment = WheelSegment(1, "Title", "Description", oldThumbnail, null)
-        val goingToBeUpdatedWheelSegment =
-            WheelSegment(1, "Title", "Description", newThumbnail, null)
+        repository.add(wheelSegment)
+        val thumbnail = wheelSegment.thumbnail!!
+        val changedWheelSegment = wheelSegment.copy(thumbnail = thumbnail.copy(id = "otherId.png"))
 
-        coEvery { repository.get(segmentId = 1) } returns savedWheelSegment
-        coEvery { repository.update(goingToBeUpdatedWheelSegment) } just runs
-        coEvery { imageScaler.scale(newThumbnail, any()) } returns newThumbnail
+        coEvery { imageScaler.scale(any(), any()) } returns thumbnail
 
         // When - invoke update
-        updateWheelSegmentUseCase(
-            wheelSegmentId = 1,
-            transform = { goingToBeUpdatedWheelSegment }
-        )
+        updateWheelSegmentUseCase(changedWheelSegment)
+
         // Then - verify scale was called
         coVerify { imageScaler.scale(any(), any()) }
     }
