6320d82b76dbfb8f73b8f887f2a570d24566d433
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-02-12 13:06:32 +0700

    feat(domain): fix thumbnail before saving

--- domain/src/main/java/com/random/randomizer/domain/usecase/CreateWheelSegmentUseCase.kt
+++ domain/src/main/java/com/random/randomizer/domain/usecase/CreateWheelSegmentUseCase.kt
@@ -2,12 +2,23 @@ package com.random.randomizer.domain.usecase
 
 import com.random.randomizer.domain.model.WheelSegment
 import com.random.randomizer.domain.repository.WheelSegmentRepository
+import java.io.File
 import javax.inject.Inject
 
 class CreateWheelSegmentUseCase @Inject constructor(
     private val wheelSegmentRepository: WheelSegmentRepository
 ) {
     suspend operator fun invoke(wheelSegment: WheelSegment) {
-        wheelSegmentRepository.add(wheelSegment)
+        wheelSegmentRepository.add(wheelSegment.withFixedThumbnail())
+    }
+
+    private fun WheelSegment.withFixedThumbnail(): WheelSegment {
+        return if (thumbnail == null) {
+            this
+        } else {
+            val correctThumbnailId = thumbnail.id.substringAfterLast(File.separator)
+            val correctThumbnail = thumbnail.copy(id = correctThumbnailId)
+            copy(thumbnail = correctThumbnail)
+        }
     }
 }
