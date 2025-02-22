70a33567e71f4f41a170773766e02e945393fdcf
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-02-22 14:07:55 +0700

    fix(app): repair EditWheelSegmentViewModel test

--- app/src/test/java/com/random/randomizer/presentation/screen/edit/FakeEditSegmentMappers.kt
+++ app/src/test/java/com/random/randomizer/presentation/screen/edit/FakeEditSegmentMappers.kt
@@ -6,17 +6,25 @@ import com.random.randomizer.domain.model.Image
 import com.random.randomizer.domain.model.WheelSegment
 import com.random.randomizer.presentation.core.FakeCoreMappers
 import com.random.randomizer.presentation.core.WheelSegmentUiState
+import com.random.randomizer.presentation.screen.segment.EditWheelSegmentMappers
 
-object FakeEditSegmentMappers : EditSegmentMappers {
-    override fun toPresentation(wheelSegment: WheelSegment): WheelSegmentUiState {
-        return FakeCoreMappers.toPresentation(wheelSegment)
-    }
-
+object FakeEditSegmentMappers : EditWheelSegmentMappers {
     override fun toPresentation(thumbnail: Image): ImageBitmap? {
         return FakeCoreMappers.toPresentation(thumbnail)
     }
 
+    override fun toPresentation(color: Long): Color {
+        return Color(color)
+    }
+
     override fun toDomain(color: Color): Long {
         return FakeCoreMappers.toDomain(color)
+    }
+
+    override fun toDomain(
+        wheelSegmentUiState: WheelSegmentUiState,
+        thumbnail: Image?
+    ): WheelSegment {
+        return FakeCoreMappers.toDomain(wheelSegmentUiState, thumbnail)
     }
 }
