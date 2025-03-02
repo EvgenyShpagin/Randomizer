844a18940ce16333b77ab1c50c8c95cb526b9383
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-03-02 14:06:27 +0700

    refactor(app): add unionWithWindowInsets

--- app/src/main/java/com/random/randomizer/presentation/core/PaddingUtils.kt
+++ app/src/main/java/com/random/randomizer/presentation/core/PaddingUtils.kt
@@ -2,10 +2,16 @@ package com.random.randomizer.presentation.core
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.coerceAtLeast
 
 
 fun Modifier.combinePaddingWithInsets(
@@ -16,4 +22,24 @@ fun Modifier.combinePaddingWithInsets(
         .padding(padding)
         .consumeWindowInsets(padding)
         .windowInsetsPadding(insets)
+}
+
+@Composable
+fun PaddingValues.unionWithWindowInsets(windowInsets: WindowInsets): PaddingValues {
+    val layoutDirection = LocalLayoutDirection.current
+    val windowInsetsPadding = windowInsets.asPaddingValues()
+    return PaddingValues(
+        start = windowInsetsPadding
+            .calculateStartPadding(layoutDirection)
+            .coerceAtLeast(calculateStartPadding(layoutDirection)),
+        top = windowInsetsPadding
+            .calculateTopPadding()
+            .coerceAtLeast(calculateTopPadding()),
+        end = windowInsetsPadding
+            .calculateEndPadding(layoutDirection)
+            .coerceAtLeast(calculateEndPadding(layoutDirection)),
+        bottom = windowInsetsPadding
+            .calculateBottomPadding()
+            .coerceAtLeast(calculateBottomPadding())
+    )
 }
