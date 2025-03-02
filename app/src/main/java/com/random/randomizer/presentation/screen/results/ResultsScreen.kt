e9e94a2ee96c15a2045fe44d64f0c530bbc3cef4
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-03-02 13:46:33 +0700

    refactor(app): simplify insets in ResultsScreen

--- app/src/main/java/com/random/randomizer/presentation/screen/results/ResultsScreen.kt
+++ app/src/main/java/com/random/randomizer/presentation/screen/results/ResultsScreen.kt
@@ -2,27 +2,23 @@ package com.random.randomizer.presentation.screen.results
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.calculateEndPadding
-import androidx.compose.foundation.layout.calculateStartPadding
-import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.random.randomizer.presentation.core.WheelSegment
 import com.random.randomizer.presentation.core.WheelSegmentUiState
+import com.random.randomizer.presentation.core.combinePaddingWithInsets
 import com.random.randomizer.presentation.theme.AppTheme
 import com.random.randomizer.presentation.util.HandleUiEffects
 
@@ -45,28 +41,13 @@ fun ResultsScreen(
         navigateToHome()
     }
 
-    val layoutDirection = LocalLayoutDirection.current
-
-    val safeContentPadding = WindowInsets.systemBars.asPaddingValues()
-    val topPadding = safeContentPadding.calculateTopPadding()
-    val bottomPadding = safeContentPadding.calculateBottomPadding()
-    val startPadding = safeContentPadding.calculateStartPadding(layoutDirection)
-    val endPadding = safeContentPadding.calculateEndPadding(layoutDirection)
-
     ResultsContent(
         winnerWheelSegment = uiState.winnerUiState,
         canDeleteWinner = uiState.canDeleteWinner,
         navigateBack = navigateToHome,
         onSpinClicked = { viewModel.onEvent(ResultsUiEvent.Spin) },
         onDeleteAndSpinClicked = { viewModel.onEvent(ResultsUiEvent.SpinAndDelete) },
-        modifier = modifier
-            .padding(
-                top = topPadding,
-                start = startPadding,
-                end = endPadding,
-                bottom = bottomPadding
-            )
-            .consumeWindowInsets(safeContentPadding)
+        modifier = modifier.systemBarsPadding()
     )
 }
 
@@ -79,13 +60,10 @@ fun ResultsContent(
     onDeleteAndSpinClicked: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val layoutDirection = LocalLayoutDirection.current
-    val displayCutout = WindowInsets.displayCutout.asPaddingValues()
-    val cutoutStartPadding = displayCutout.calculateStartPadding(layoutDirection)
-    val cutoutEndPadding = displayCutout.calculateEndPadding(layoutDirection)
-
-    val startPadding = cutoutStartPadding.coerceAtLeast(16.dp)
-    val endPadding = cutoutEndPadding.coerceAtLeast(16.dp)
+    val horizontalPaddingModifier = Modifier.combinePaddingWithInsets(
+        padding = PaddingValues(horizontal = 16.dp),
+        insets = WindowInsets.displayCutout
+    )
 
     Box(modifier = modifier.fillMaxSize()) {
         GradientBackground(modifier = Modifier.fillMaxSize())
@@ -100,10 +78,7 @@ fun ResultsContent(
             onClick = {},
             modifier = Modifier
                 .align(Alignment.Center)
-                .padding(
-                    start = startPadding,
-                    end = endPadding
-                )
+                .then(horizontalPaddingModifier)
         )
         SpinButtons(
             onSpinClicked = onSpinClicked,
@@ -111,11 +86,8 @@ fun ResultsContent(
             showDeleteButton = canDeleteWinner,
             modifier = Modifier
                 .align(Alignment.BottomCenter)
-                .padding(
-                    bottom = 16.dp,
-                    start = startPadding,
-                    end = endPadding
-                )
+                .then(horizontalPaddingModifier)
+                .padding(bottom = 16.dp)
         )
     }
 }
