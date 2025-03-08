bf2a5c40b8ba4796513e95a3e9763370bd440797
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-03-08 18:09:31 +0700

    feat(app): add simple appear animation

--- app/src/main/java/com/random/randomizer/presentation/screen/results/ResultsScreen.kt
+++ app/src/main/java/com/random/randomizer/presentation/screen/results/ResultsScreen.kt
@@ -1,6 +1,12 @@
 package com.random.randomizer.presentation.screen.results
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
@@ -11,6 +17,7 @@ import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
@@ -65,14 +72,33 @@ fun ResultsContent(
         insets = WindowInsets.displayCutout
     )
 
+    val gradientAnimationState = remember {
+        MutableTransitionState(false)
+            // Start the animation immediately.
+            .apply { targetState = true }
+    }
+
     Surface(modifier = modifier) {
         Box(
             modifier = Modifier
                 .systemBarsPadding()
                 .fillMaxSize()
         ) {
-            GradientBackground(modifier = Modifier.fillMaxSize())
-
+            AnimatedVisibility(
+                visibleState = gradientAnimationState,
+                enter = fadeIn(animationSpec = tween(500)) +
+                        expandVertically(
+                            animationSpec = tween(1000),
+                            expandFrom = Alignment.CenterVertically
+                        ),
+                exit = ExitTransition.None,
+                modifier = Modifier.align(Alignment.Center)
+            ) {
+                GradientBackground(
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+            }
             ResultsTopAppBar(
                 onNavigationClick = navigateBack,
                 modifier = Modifier.align(Alignment.TopCenter)
