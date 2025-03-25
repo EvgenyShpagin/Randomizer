07bccb861caaee239a6d3a4d8f936a19bb5c629e
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-03-25 17:29:20 +0700

    refactor(app): distribute into different files

--- app/src/main/java/com/random/randomizer/presentation/util/LifecycleUtils.kt
+++ app/src/main/java/com/random/randomizer/presentation/util/LifecycleUtils.kt
@@ -1,24 +1,14 @@
 package com.random.randomizer.presentation.util
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.flowWithLifecycle
-import com.random.randomizer.presentation.core.UiEffect
 import kotlinx.coroutines.flow.Flow
 
 @Composable
-fun <E : UiEffect> HandleUiEffects(flow: Flow<E>, onReceive: suspend (E) -> Unit) {
-    val uiEffect = rememberFlowWithLifecycle(flow)
-    LaunchedEffect(uiEffect) {
-        uiEffect.collect { onReceive(it) }
-    }
-}
-
-@Composable
-private fun <T> rememberFlowWithLifecycle(
+fun <T> rememberFlowWithLifecycle(
     flow: Flow<T>,
     lifecycle: Lifecycle = LocalLifecycleOwner.current.lifecycle
 ): Flow<T> {
---
+++ app/src/main/java/com/random/randomizer/presentation/util/MviUtils.kt
@@ -0,0 +1,14 @@
+package com.random.randomizer.presentation.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import com.random.randomizer.presentation.core.UiEffect
+import kotlinx.coroutines.flow.Flow
+
+@Composable
+fun <E : UiEffect> HandleUiEffects(flow: Flow<E>, onReceive: suspend (E) -> Unit) {
+    val uiEffect = rememberFlowWithLifecycle(flow)
+    LaunchedEffect(uiEffect) {
+        uiEffect.collect { onReceive(it) }
+    }
+}
