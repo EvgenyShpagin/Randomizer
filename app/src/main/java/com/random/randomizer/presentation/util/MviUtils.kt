90ca5251007eabb5de9b55f3cf19bf5d33731dd0
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-03-31 19:24:20 +0700

    refactor(app): use rememberUpdatedState

--- app/src/main/java/com/random/randomizer/presentation/util/MviUtils.kt
+++ app/src/main/java/com/random/randomizer/presentation/util/MviUtils.kt
@@ -2,13 +2,16 @@ package com.random.randomizer.presentation.util
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import com.random.randomizer.presentation.core.UiEffect
 import kotlinx.coroutines.flow.Flow
 
 @Composable
 fun <E : UiEffect> HandleUiEffects(flow: Flow<E>, onReceive: suspend (E) -> Unit) {
     val uiEffect = rememberFlowWithLifecycle(flow)
+    val currentOnReceive by rememberUpdatedState(onReceive)
     LaunchedEffect(uiEffect) {
-        uiEffect.collect { onReceive(it) }
+        uiEffect.collect { currentOnReceive(it) }
     }
 }
