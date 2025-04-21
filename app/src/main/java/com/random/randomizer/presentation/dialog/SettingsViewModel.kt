cad88196e4456b8eae6f8eae49db40e2c1908c24
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-04-21 15:18:06 +0700

    feat(app): implement SettingsViewModel

--- app/src/main/java/com/random/randomizer/presentation/dialog/SettingsViewModel.kt
+++ app/src/main/java/com/random/randomizer/presentation/dialog/SettingsViewModel.kt
@@ -1,15 +1,46 @@
 package com.random.randomizer.presentation.dialog
 
+import androidx.lifecycle.viewModelScope
+import com.random.randomizer.data.model.AndroidThemeOption
+import com.random.randomizer.domain.usecase.GetThemeOptionsStreamUseCase
+import com.random.randomizer.domain.usecase.SetThemeOptionUseCase
 import com.random.randomizer.presentation.core.ImmutableStateViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewModel @Inject constructor() : ImmutableStateViewModel<SettingsUiState, SettingsUiEvent, Nothing>() {
-    override val uiState: StateFlow<SettingsUiState> = TODO("Not yet implemented")
+class SettingsViewModel @Inject constructor(
+    getThemeOptionsStreamUseCase: GetThemeOptionsStreamUseCase,
+    private val setThemeOptionUseCase: SetThemeOptionUseCase
+) : ImmutableStateViewModel<SettingsUiState, SettingsUiEvent, Nothing>() {
+
+    override val uiState: StateFlow<SettingsUiState> = getThemeOptionsStreamUseCase()
+        .map { themeOptions ->
+            val colorScheme = themeOptions.find { it is AndroidThemeOption.ColorScheme }
+            val darkModeConfig = themeOptions.find { it is AndroidThemeOption.DarkModeConfig }
+            SettingsUiState(
+                isLoading = false,
+                colorScheme = colorScheme as AndroidThemeOption.ColorScheme,
+                darkModeConfig = darkModeConfig as AndroidThemeOption.DarkModeConfig
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = SettingsUiState()
+        )
 
     override fun onEvent(event: SettingsUiEvent) {
-        TODO("Not yet implemented")
+        viewModelScope.launch {
+            when (event) {
+                is SettingsUiEvent.SetColorScheme -> setThemeOptionUseCase(event.colorScheme)
+                is SettingsUiEvent.SetDarkModeConfig -> setThemeOptionUseCase(event.darkModeConfig)
+            }
+        }
     }
 }
